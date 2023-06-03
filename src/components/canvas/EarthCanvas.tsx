fc8fd8db98d3379e5fc891583dca57c0f7586878
Austin <ndkhiem3010@gmail.com>
2023-06-04 00:54:35 +0800

    Building EarthCanvas in Contact section

--- src/components/canvas/EarthCanvas.tsx
+++ src/components/canvas/EarthCanvas.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
+import CanvasLoader from "../CanvasLoader";
+
+// Component: Earth
+// This component represents the Earth model rendered in the scene.
+const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf"); // Load the Earth model using the useGLTF hook
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    // Render the Earth model as a primitive object with specified scale, position, and rotation
+  );
+};
+
+// Component: EarthCanvas
+// This component represents the canvas where the 3D scene is rendered.
 const EarthCanvas = () => {
-  return <div>EarthCanvas</div>;
+  return (
+    <Canvas
+      shadows // Enable shadow rendering
+      frameloop="demand" // Control the frame rendering manually
+      gl={{ preserveDrawingBuffer: true }} // Preserve the drawing buffer for screenshot capture
+      camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }} // Camera configuration
+    >
+      {/* Display fallback content until the scene is loaded */}
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate // Enable automatic rotation of the camera around the target
+          enableZoom={false} // Disable zooming with the mouse scroll
+          maxPolarAngle={Math.PI / 2} // Limit the maximum angle of the camera's polar rotation
+          minPolarAngle={Math.PI / 2} // Limit the minimum angle of the camera's polar rotation
+        />
+        {/* Render the Earth component */}
+        <Earth />
+      </Suspense>
+      {/* Preload all assets before showing the scene */}
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default EarthCanvas;
