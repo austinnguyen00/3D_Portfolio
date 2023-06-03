995875827bf79589fffc63d3dc74bec947b402f1
Austin <ndkhiem3010@gmail.com>
2023-06-03 17:23:58 +0800

    Removing StarsCanvas duplicate component

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,6 +1,6 @@
 import EarthCanvas from "./EarthCanvas";
 import BallCanvas from "./BallCanvas";
 import ComputersCanvas from "./ComputersCanvas";
-import StarsCanvas from "../StarsCanvas";
+import StarsCanvas from "./StarsCanvas";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
