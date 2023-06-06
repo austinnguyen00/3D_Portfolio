6057d8705fb28de34d1c654c2dc6c61f12de8753
Austin <ndkhiem3010@gmail.com>
2023-06-06 21:49:24 +0800

    Update Experience Section to Growth Journey section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,17 @@
-import logo from "./logo.svg";
 import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
-import menu from "./menu.svg";
+import carrent from "./carrent.png";
 import close from "./close.svg";
-
+import hackthebox from "./company/hackthebox.png";
+import kaggle from "./company/kaggle.png";
+import react from "./company/react.png";
+import uwa from "./company/uwa.png";
+import wordfixerbot from "./company/wordfixerbot.png";
+import creator from "./creator.png";
+import github from "./github.png";
+import jobit from "./jobit.png";
+import logo from "./logo.svg";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -18,17 +23,10 @@ import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.png";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
+import typescript from "./tech/typescript.png";
 import tripguide from "./tripguide.png";
+import web from "./web.png";
 
 export {
   logo,
@@ -52,11 +50,12 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
+  wordfixerbot,
+  kaggle,
+  react,
+  uwa,
+  hackthebox,
 };
