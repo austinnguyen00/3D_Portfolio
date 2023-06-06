8dcfae541c7ff188fb8dc537e75a821b9a94c3fd
Austin <ndkhiem3010@gmail.com>
2023-06-06 19:48:16 +0800

    Remove Testimonial section and delete Feedback component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
+
 import {
   About,
   Contact,
   Experience,
-  Feedbacks,
   Hero,
   Navbar,
+  StarsCanvas,
   Tech,
   Works,
-  StarsCanvas,
 } from "./components";
 
 const App = () => {
@@ -23,7 +23,6 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,12 @@
+import About from "./About";
 import {
-  EarthCanvas,
   BallCanvas,
   ComputersCanvas,
+  EarthCanvas,
   StarsCanvas,
 } from "./canvas";
-import About from "./About";
 import Contact from "./Contact";
 import Experience from "./Experience";
-import Feedbacks from "./Feedbacks";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Tech from "./Tech";
@@ -17,7 +16,6 @@ export {
   About,
   Contact,
   Experience,
-  Feedbacks,
   Hero,
   Navbar,
   Tech,
